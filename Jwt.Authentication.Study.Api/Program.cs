13ddb99d32ebfb8ac288d3ab58f23859438faf7e
Renan Perez <renan.perez@ambevtech.com.br>
2023-10-16 22:04:42 -0300

    refactor: remove comments

--- Jwt.Authentication.Study.Api/Program.cs
+++ Jwt.Authentication.Study.Api/Program.cs
@@ -3,15 +3,11 @@ using Jwt.Authentication.Study.Api.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
 
 builder.Services.RegisterApplication(builder.Configuration);
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
 
